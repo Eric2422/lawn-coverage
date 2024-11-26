f00a04ce994311b7482aeba47cb0cddc9d8a7aed
Eric Zhang <ericbkzhang@gmail.com>
2024-11-26 09:21:09 -0500

    Add grid

--- src/main.py
+++ src/main.py
@@ -1,4 +1,31 @@
 import cv2
-import numpy
-from matplotlib import pyplot
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import image as mpimg
+from matplotlib import ticker
+from PIL import Image
 
+img = Image.open('img/e_coli_90s.jpg')
+print(img.size)
+
+dpi = 300
+
+fig = plt.figure(figsize=(float(img.size[0] / dpi), float(img.size[1]) / dpi))
+ax = fig.add_subplot(111)
+
+# Remove whitespace from around the image
+fig.subplots_adjust(left=0,right=1,bottom=0,top=1)
+
+interval = img.size[0] / 100
+loc = ticker.MultipleLocator(base=interval)
+ax.xaxis.set_major_locator(loc)
+ax.yaxis.set_major_locator(loc)
+
+# Add the grid
+ax.grid(which='major', axis='both', linestyle='-')
+
+ax.imshow(img)
+plt.show()
+
+# Save the figure
+# fig.savefig('myImageGrid.tiff')
