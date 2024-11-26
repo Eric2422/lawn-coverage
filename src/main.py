ff931dde49ae5a62998866e45f88be989114f448
Eric Zhang <ericbkzhang@gmail.com>
2024-11-26 11:24:09 -0500

    Adjust grid to divide image into hundredths

--- src/main.py
+++ src/main.py
@@ -6,17 +6,17 @@ from matplotlib import ticker
 from PIL import Image
 
 img = Image.open('img/e_coli_90s.jpg')
-print(img.size)
+print(img.info.get('dpi'))
 
-dpi = 300
+dpi = 100
 
-fig = plt.figure(figsize=(float(img.size[0] / dpi), float(img.size[1]) / dpi))
+fig = plt.figure(figsize=(float(img.size[0]), float(img.size[1])))
 ax = fig.add_subplot(111)
 
 # Remove whitespace from around the image
 fig.subplots_adjust(left=0,right=1,bottom=0,top=1)
 
-interval = img.size[0] / 100
+interval = img.size[0] / 10
 loc = ticker.MultipleLocator(base=interval)
 ax.xaxis.set_major_locator(loc)
 ax.yaxis.set_major_locator(loc)
@@ -26,6 +26,3 @@ ax.grid(which='major', axis='both', linestyle='-')
 
 ax.imshow(img)
 plt.show()
-
-# Save the figure
-# fig.savefig('myImageGrid.tiff')
