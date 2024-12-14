4e60cfd37aad1338688cb4920a99f27fc0134599
Eric Zhang <ericbkzhang@gmail.com>
2024-12-14 10:20:16 -0500

    Add path

--- src/main.py
+++ src/main.py
@@ -1,30 +1,40 @@
+from pathlib import Path
+
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import ticker
+
 from PIL import Image
 from PyQt5.QtWidgets import QApplication
-import sys
 
-plt_img = Image.open('img/e_coli_90s.jpg')
-print(plt_img.info.get('dpi'))
+IMG_DIR = Path('./')
 
-dpi = 100
 
-fig = plt.figure(figsize=(float(plt_img.size[0]), float(plt_img.size[1])))
-ax = fig.add_subplot(111)
+def show_img():
+    plt_img = Image.open('img/e_coli_90s.jpg')
+    print(plt_img.info.get('dpi'))
 
-# Remove whitespace from around the image
-fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+    dpi = 100
 
-# Create a grid that divide the image into hundredths
-interval = plt_img.size[0] / 10
-loc = ticker.MultipleLocator(base=interval)
-ax.xaxis.set_major_locator(loc)
-ax.yaxis.set_major_locator(loc)
+    fig = plt.figure(figsize=(float(plt_img.size[0]), float(plt_img.size[1])))
+    ax = fig.add_subplot(111)
 
-# Add the grid
-ax.grid(which='major', axis='both', linestyle='-')
+    # Remove whitespace from around the image
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
 
-ax.imshow(plt_img)
-plt.show()
+    # Create a grid that divide the image into hundredths
+    interval = plt_img.size[0] / 10
+    loc = ticker.MultipleLocator(base=interval)
+    ax.xaxis.set_major_locator(loc)
+    ax.yaxis.set_major_locator(loc)
+
+    # Add the grid
+    ax.grid(which='major', axis='both', linestyle='-')
+
+    ax.imshow(plt_img)
+    plt.show()
+
+
+cv2.imgread()
+show_img()
